9cba132b22de1ab53f4f61c82d73a77a8a2a9a55
Luz Alba Posse <luzalbaposse@gmail.com>
2023-11-22 18:06:42 -0300

    Update Titulo.jsx

---
+++ las-que-deciden/app/componentes/Titulo.jsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function Home() {
+    <div className=''>
+        <Image 
+        src='/logo.png' 
+        alt='logo' 
+        width={200} 
+        height={200} 
+        />
+    </div>
+}
+
+
