3f5ee007ced50445cf3891c72254b0ef3bf657fc
Luz Alba Posse <luzalbaposse@gmail.com>
2023-11-23 12:28:02 -0300

    Update Derechos.jsx

--- las-que-deciden/app/componentes/Derechos.jsx
+++ las-que-deciden/app/componentes/Derechos.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 export default function Derechos(){
     return (
+
 <div className="max-w-full text-center text-red-600">
             <span className="text-2xl font-light leading-snug">LA HISTORIA<br/></span>
             <span className="text-6xl font-bold leading-tight">SOBRE DERECHOS Y REVOLUCIÓN</span>
@@ -11,6 +12,7 @@ export default function Derechos(){
 <div className="text-center text-black text-2xl font-light tracking-wide mb-8">
 La participación evoluciona junto al reconocimiento de derechos a lo largo de la historia. El más relevante: el derecho al voto.
 </div>   
-      
-    )
+</div>
+
+    );
 }
